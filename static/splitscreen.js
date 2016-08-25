a867de50e73b9156561c6960adc2caa65aea59a6
MathildeJ <mathilde.jean@depinfonancy.net>
2016-08-25 13:57:32 +0200

    cleaning up

--- static/splitscreen.js
+++ static/splitscreen.js
@@ -119,17 +119,3 @@ window.addEventListener('message', function(e){
      }
 })
 
-// specify form behaviour
-$(document).ready(function(){
-	$('#url').focus(function() { 
-  		$(this).val(''); 
-	});
-	$('#url').keypress(function(e){
-		if (e.keyCode == 13){
-			e.preventDefault();
-			$('#submit_button').click();
-		}
-	});
-
-});
-
